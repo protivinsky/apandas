2c0ae140716937439568a5ba9f692c00a4b7c3ca
Tomas Protivinsky <tomas.protivinsky@gmail.com>
2023-06-18 15:19:12 +0000

    mostly done with support for groupby

--- apandas/acolumn.py
+++ apandas/acolumn.py
@@ -15,6 +15,7 @@ class AFunction:
         return self.from_frame(af)
 
     def __add__(self, other):
+        # this can be done differently, in metaclass or otherwise
         return AFunction.apply_operator(operator.add, self, other)
 
     def __sub__(self, other):
@@ -26,9 +27,30 @@ class AFunction:
     def __truediv__(self, other):
         return AFunction.apply_operator(operator.truediv, self, other)
 
+    def __lt__(self, other):
+        return AFunction.apply_operator(operator.lt, self, other)
+
+    def __gt__(self, other):
+        return AFunction.apply_operator(operator.gt, self, other)
+
+    def __le__(self, other):
+        return AFunction.apply_operator(operator.le, self, other)
+
+    def __ge__(self, other):
+        return AFunction.apply_operator(operator.ge, self, other)
+
+    def __eq__(self, other):
+        return AFunction.apply_operator(operator.eq, self, other)
+
+    def __ne__(self, other):
+        return AFunction.apply_operator(operator.ne, self, other)
+
     @staticmethod
     def apply_operator(op, *args):
         return AFunction(lambda af: op(*[(x.from_frame(af) if isinstance(x, AFunction) else x) for x in args]))
+
+    def __hash__(self):
+        return hash(self.func)
 
 
 class AColumn(AFunction):
@@ -48,3 +70,6 @@ class AColumn(AFunction):
 
     def __repr__(self):
         return f"AColumn['{self.name}']"
+
+    def __hash__(self):
+        return hash(self.name) ^ hash(self.func)

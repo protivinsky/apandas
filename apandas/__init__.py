7eb743c3afebb825d7c77d37f2f747b7352b666f
Tomas Protivinsky <tomas.protivinsky@gmail.com>
2023-06-24 17:31:59 +0000

    doc fix

--- apandas/__init__.py
+++ apandas/__init__.py
@@ -13,5 +13,12 @@ APandas: Lightweight wrapper to support custom analytics in Pandas
 ==================================================================
 """
 
-with open(os.path.join('..', 'README.rst'), 'r') as f:
+# Get the current file's directory
+_here = os.path.abspath(os.path.dirname(__file__))
+# And path to README.rst
+_readme_path = os.path.join(_here, '..', 'README.rst')
+
+# Construct the path to the README file in the parent directory
+with open(_readme_path, 'r') as f:
     __doc__ = f.read().replace(_remove_from_doc, '')
+
